a0d52ef1f1a0cddfa3fbf318b3663cec501f957c
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-07 01:49:12 +0200

    Added skeleton to load all owners list unto index.html | Closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/controller/OwnerController.java
@@ -1,7 +1,10 @@
 package guru.springframework.shayan.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.shayan.sfgpetclinic.service.OwnerService;
 
 /**
  * Created by Shayan Ahmad (sahmed) on 29-Nov-19.
@@ -10,8 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("ownersList", ownerService.findAll());
+
         return "owners/index";
     }
 
