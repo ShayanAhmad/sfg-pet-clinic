5f336b8845fda0ac193538b9a1177c58a0093e5f
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-07 01:55:26 +0200

    Added skeleton to load all vets list unto index.html | Closes #15.

--- pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/controller/VetController.java
@@ -1,7 +1,10 @@
 package guru.springframework.shayan.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.shayan.sfgpetclinic.service.VetService;
 
 /**
  * Created by Shayan Ahmad (sahmed) on 29-Nov-19.
@@ -9,8 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets/index", "vets", "vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model) {
+        model.addAttribute("vetsList", vetService.findAll());
+
         return "vets/index";
     }
 
