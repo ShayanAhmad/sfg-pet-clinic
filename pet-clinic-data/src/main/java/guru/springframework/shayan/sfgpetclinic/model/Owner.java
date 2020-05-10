8e2e2fbfc6db24afeba367c05d0ff2a6e3f951e5
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-10 16:33:12 +0200

    Closes #26: 1. Added Pet association in Owner. 2. Added Visit entity.

--- pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Owner.java
@@ -1,9 +1,19 @@
 package guru.springframework.shayan.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Shayan Ahmad (sahmed) on 27-Oct-19.
  */
 public class Owner extends Person {
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
