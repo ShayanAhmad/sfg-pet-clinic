ebe58e063b4ad25efcbb65a5dcecc0a9c66b4a05
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-13 01:44:35 +0200

    1. Added name in PetType.java 2. Added some lines to load PetType in DataLoader.
    Closes #32.

--- pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/PetType.java
@@ -4,4 +4,13 @@ package guru.springframework.shayan.sfgpetclinic.model;
  * Created by Shayan Ahmad (sahmed) on 27-Oct-19.
  */
 public class PetType extends BaseEntity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springframework.shayan.sfgpetclinic.model.Owner;
+import guru.springframework.shayan.sfgpetclinic.model.PetType;
 import guru.springframework.shayan.sfgpetclinic.model.Vet;
 import guru.springframework.shayan.sfgpetclinic.service.OwnerService;
+import guru.springframework.shayan.sfgpetclinic.service.PetTypeService;
 import guru.springframework.shayan.sfgpetclinic.service.VetService;
 
 /**
@@ -16,10 +18,12 @@ public class DataLoader implements CommandLineRunner {
 
     private final VetService vetService;
     private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(VetService vetService, OwnerService ownerService) {
+    public DataLoader(VetService vetService, OwnerService ownerService, PetTypeService petTypeService) {
         this.vetService = vetService;
         this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
@@ -28,6 +32,10 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void loadDefaultData() {
+        createAndSaveDummyPetType("Cat");
+        createAndSaveDummyPetType("Dog");
+        System.out.println("Loaded Pet types!");
+
         createAndSaveDummyOwner("Andrew", "Bee");
         createAndSaveDummyOwner("Bruce", "Lee");
         System.out.println("Owners loaded!");
@@ -35,6 +43,12 @@ public class DataLoader implements CommandLineRunner {
         createAndSaveDummyVet("Dr. Aby", "Neil");
         createAndSaveDummyVet("Dr. Koki", "Na-Basu");
         System.out.println("Vets loaded!");
+    }
+
+    private void createAndSaveDummyPetType(String petName) {
+        PetType petType = new PetType();
+        petType.setName(petName);
+        petTypeService.save(petType);
     }
 
     private void createAndSaveDummyOwner(String fName, String lName) {
