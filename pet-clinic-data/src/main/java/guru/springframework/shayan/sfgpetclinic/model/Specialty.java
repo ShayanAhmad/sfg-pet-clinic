e84503ffc57091896708f2c22a4ac11895b294a4
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-13 00:43:52 +0200

    Closes #27: 1. Added Specialty for Vet. 2. Created association between Vet and Specialty.

---
+++ pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,17 @@
+package guru.springframework.shayan.sfgpetclinic.model;
+
+/**
+ * Vet will have a specialty to handle the Pets.
+ * This class denotes that specialty characteristics.
+ */
+public class Specialty {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Vet.java
@@ -1,8 +1,19 @@
 package guru.springframework.shayan.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Shayan Ahmad (sahmed) on 27-Oct-19.
  */
 public class Vet extends Person {
 
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
