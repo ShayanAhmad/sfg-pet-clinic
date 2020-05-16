835ba07a1f12f478cc89d2fddcca95f7821e46ff
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-16 19:00:56 +0200

    1. Fixed Pet and Owner relationship in DataLoader. Closes #33

--- pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/bootstrap/DataLoader.java
@@ -10,7 +10,6 @@ import guru.springframework.shayan.sfgpetclinic.model.Pet;
 import guru.springframework.shayan.sfgpetclinic.model.PetType;
 import guru.springframework.shayan.sfgpetclinic.model.Vet;
 import guru.springframework.shayan.sfgpetclinic.service.OwnerService;
-import guru.springframework.shayan.sfgpetclinic.service.PetService;
 import guru.springframework.shayan.sfgpetclinic.service.PetTypeService;
 import guru.springframework.shayan.sfgpetclinic.service.VetService;
 
@@ -23,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final OwnerService ownerService;
     private final PetTypeService petTypeService;
-    private final PetService petService;
 
-    public DataLoader(VetService vetService, OwnerService ownerService, PetTypeService petTypeService, PetService petService) {
+    public DataLoader(VetService vetService, OwnerService ownerService, PetTypeService petTypeService) {
         this.vetService = vetService;
         this.ownerService = ownerService;
         this.petTypeService = petTypeService;
-        this.petService = petService;
     }
 
     @Override
@@ -40,29 +37,16 @@ public class DataLoader implements CommandLineRunner {
     private void loadDefaultData() {
         PetType pt1 = createAndSaveDummyPetType("Cat");
         PetType pt2 = createAndSaveDummyPetType("Dog");
-        System.out.println("Loaded Pet types!");
 
-        Owner o1 = createAndSaveDummyOwner("Andrew", "Bee", "Diatal ut.", "Debrecen", "+132456");
-        Owner o2 = createAndSaveDummyOwner("Bruce", "Lee", "Foshan", "Foshan city", "+321456");
+        createAndSaveDummyOwner("Andrew", "Bee", "Diatal ut.", "Debrecen", "+132456", "Bobby", pt1);
+        createAndSaveDummyOwner("Bruce", "Lee", "Foshan", "Foshan city", "+321456", "Rosco", pt2);
         System.out.println("Owners loaded!");
 
-        createAndSaveDummyPet(o1, pt1, "Bobby");
-        createAndSaveDummyPet(o2, pt2, "Rocco");
         System.out.println("Pets loaded!");
 
         createAndSaveDummyVet("Dr. Aby", "Neil");
         createAndSaveDummyVet("Dr. Koki", "Na-Basu");
         System.out.println("Vets loaded!");
-    }
-
-    private void createAndSaveDummyPet(Owner o1, PetType pt1, String petName) {
-        Pet pet = new Pet();
-        pet.setBirthDate(LocalDate.now());
-        pet.setName(petName);
-        pet.setPetType(pt1);
-        pet.setOwner(o1);
-
-        petService.save(pet);
     }
 
     private PetType createAndSaveDummyPetType(String petName) {
@@ -71,7 +55,7 @@ public class DataLoader implements CommandLineRunner {
         return petTypeService.save(petType);
     }
 
-    private Owner createAndSaveDummyOwner(String fName, String lName, String address, String city, String telephone) {
+    private void createAndSaveDummyOwner(String fName, String lName, String address, String city, String telephone, String petName, PetType petType) {
         Owner owner = new Owner();
         owner.setFirstName(fName);
         owner.setLastName(lName);
@@ -79,7 +63,15 @@ public class DataLoader implements CommandLineRunner {
         owner.setCity(city);
         owner.setTelephone(telephone);
 
-        return ownerService.save(owner);
+        Pet pet = new Pet();
+        pet.setBirthDate(LocalDate.now());
+        pet.setName(petName);
+        pet.setPetType(petType);
+        pet.setOwner(owner);
+
+        owner.getPets().add(pet);
+
+        ownerService.save(owner);
     }
 
     private void createAndSaveDummyVet(String fName, String lName) {
