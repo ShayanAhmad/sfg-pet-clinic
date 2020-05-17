137268d0bca7aa6c43a1bce76da2fb4f24bf3db2
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-17 16:22:07 +0200

    1. Added `not implemented` controller and html. Closes #45

--- pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/shayan/sfgpetclinic/controller/VetController.java
@@ -18,7 +18,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"vets/index", "vets", "vets/index.html"})
+    @RequestMapping({"vets/index", "vets", "vets/index.html", "vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vetsList", vetService.findAll());
 
