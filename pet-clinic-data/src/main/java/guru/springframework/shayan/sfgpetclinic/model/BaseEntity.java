7e833d393b1358210254a0c4ab97026bc43c533a
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-27 17:06:53 +0200

    1. Added JPA annotations for BaseEntity. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/BaseEntity.java
@@ -2,11 +2,19 @@ package guru.springframework.shayan.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by Shayan Ahmad (sahmed) on 03-Nov-19.
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
