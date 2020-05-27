98488537ad892242aa41750641313ed915822b60
Shayan Ahmad <v-shahmad@hotels.com>
2020-05-27 18:53:15 +0200

    1. Added JPA modeling to specialty and vet. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Specialty.java
@@ -1,10 +1,18 @@
 package guru.springframework.shayan.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Vet will have a specialty to handle the Pets.
  * This class denotes that specialty characteristics.
  */
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/shayan/sfgpetclinic/model/Vet.java
@@ -3,11 +3,24 @@ package guru.springframework.shayan.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 /**
  * Created by Shayan Ahmad (sahmed) on 27-Oct-19.
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
